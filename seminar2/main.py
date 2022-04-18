5f431b0a2c2cf51e26212542ce680986dced96ab
Olga <wiut.tutor@gmail.com>
2022-04-18 20:35:51 +0500

    first commit, seminar2

---
+++ seminar2/main.py
@@ -0,0 +1,32 @@
+# Task 5
+# num = int(input("Enter the number: "))
+# print(num)
+# print(oct(num))
+# print(hex(num))
+# Task 6
+# num = int(input("Enter the number: "))
+# if num % 2 == 1:
+#     print("odd")
+# else:
+#     print("even")
+#
+# def converter(num):
+#     print(num)
+#     print(oct(num))
+#     print(hex(num))
+#
+#
+# # num = int(input("Enter the number: "))
+# converter(10)
+# converter(111)
+
+#function  definition  with parameters
+def even_odd(number):
+    if number % 2 == 1:
+        print(number, " is odd")
+    else:
+        print(number, " is even")
+
+
+num = int(input("Enter the number: "))
+print(even_odd(num)) #function argument is num, even_odd() function call takes place here
