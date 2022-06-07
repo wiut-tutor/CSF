f8cb845e3508751c31687900c6aab995a39594b2
wiut-tutor <wiut.tutor@gmail.com>
2022-06-07 14:08:16 +0500

    Task 5, colours list

---
+++ seminar4/seminar4.py
@@ -0,0 +1,41 @@
+color_list = ["Red","White", "Black"]
+print(color_list[0])
+print(color_list[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+# color_list = ["Red", "Green", "White", "Black"]
+# print((color_list[0], color_list[-1]))
+#
+# i = 1
+# while i <=10:
+#     print(i*"*")
+#     i+=1
+#
+# i = 10
+# while i>=0:
+#     print(i*"*")
+#     i-=1
+#
+#
+# random = 8
+# while True:
+#    line = int(input('Your guess:'))
+#    if line == random:
+#        print('Correct')
+#        break
+#    print(line)
+
